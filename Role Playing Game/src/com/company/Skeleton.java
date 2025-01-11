1f0a94b8b6357af952bb1b4b3b7ac46856722796
Maurina <elena_maurina@mail.ru>
2025-01-11 21:48:55 +0300

    Role Playing Game

---
+++ Role Playing Game/src/com/company/Skeleton.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Skeleton extends FantasyCharacter {
+    public Skeleton(String name, int healthPoints, int strength, int dexterity, int xp, int gold) {
+        super(name, healthPoints, strength, dexterity, xp, gold);
+    }
+}
